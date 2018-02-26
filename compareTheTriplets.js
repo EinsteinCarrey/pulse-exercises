b0bd8a456050179249677daca2b00419f83668f4
Einstein <forensicslabnet@gmail.com>
2018-02-26 13:06:15 +0300

    Adds functionality for comparing triplets

--- compareTheTriplets.js
+++ compareTheTriplets.js
@@ -1,4 +1,28 @@
-function sum(a, b) {
-    return a + b;
+function compareTriplets(a, b) {
+    a = a.split(' ');
+    b = b.split(' ');
+
+    /* Verify all inputs have three items each */
+    if (a.length !== 3 || b.length !== 3){
+        return "Both parameters must have 3 items each";
+    }
+
+    /* Verify that all items are integers */
+    if(isNaN(a.join("") + b.join(""))){
+        return "Both parameters must contain numbers only";
+    }
+
+    let output = [0, 0];
+
+    a.map((value, index)=>{
+        if(value === b[index]) return;
+
+        parseInt(value) > parseInt(b[index]) ? output[0] +=1 : output[1] +=1;
+
+    });
+
+    return output.join(" ");
+
 }
-module.exports = sum;
+
+module.exports = compareTriplets;
--- tests/compareTheTriplets.test.js
+++ tests/compareTheTriplets.test.js
@@ -1,5 +1,25 @@
-const sum = require('../compareTheTriplets');
+describe('Tests cases for compareTheTriplets', () => {
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    const compareTheTriplets = require('../compareTheTriplets');
+    const anne = "5 6 7";
+    const bob = "3 6 10";
+    const anne2 = "3 6 10";
+    const bob2 = "6 10 14";
+
+    test('Both Anne and Bob earn 1 point each', () => {
+        expect(compareTheTriplets(anne, bob)).toBe("1 1");
+    });
+
+    test('Both Anne and Bob earn 1 point each', () => {
+        expect(compareTheTriplets(bob, anne)).toBe("1 1");
+    });
+
+    test('Both Anne and Bob don not earn any points', () => {
+        expect(compareTheTriplets(anne2, bob)).toBe("0 0");
+    });
+
+    test('Bob2 earn more points than Anne2', () => {
+        expect(compareTheTriplets(anne2, bob2)).toBe("0 3");
+    });
+
 });
