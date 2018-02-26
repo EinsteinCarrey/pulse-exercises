8f72587ae4cf3eaf3e49f0657c172066f40afedd
Einstein <forensicslabnet@gmail.com>
2018-02-26 19:54:57 +0300

    [Fix] handled edge case constraints

--- src/compareTheTriplets.js
+++ src/compareTheTriplets.js
@@ -12,6 +12,20 @@ function compareTriplets(a, b) {
         return "Both parameters must contain numbers only";
     }
 
+    /* Verify that all items are integers less than 101 */
+    const aIndex = a.findIndex(x => x > 100);
+    const bIndex = b.findIndex(x => x > 100);
+    if(aIndex > -1 || bIndex > -1) {
+        return "All numbers in the parameter must less than or equal to 100";
+    }
+
+    /* Verify that all items are positive integers*/
+    const indexA = a.findIndex(x => x < 0);
+    const indexB = b.findIndex(x => x < 0);
+    if(indexA > -1 || indexB > -1) {
+        return "All numbers in the parameter must be positive integers";
+    }
+
     let output = [0, 0];
 
     a.map((value, index)=>{
--- tests/compareTheTriplets.test.js
+++ tests/compareTheTriplets.test.js
@@ -1,10 +1,14 @@
 describe("Tests cases for compareTheTriplets", () => {
 
-    const compareTheTriplets = require("../compareTheTriplets");
+    const compareTheTriplets = require("../src/compareTheTriplets");
     const anne = "5 6 7";
     const bob = "3 6 10";
     const anne2 = "3 6 10";
     const bob2 = "6 10 14";
+    const anne3 = "2000 6 10";
+    const bob3 = "1000 10 14";
+    const anne4 = "-1 6 10";
+    const bob4 = "5 10 14";
 
     test("Both Anne and Bob earn 1 point each", () => {
         expect(compareTheTriplets(anne, bob)).toBe("1 1");
@@ -20,6 +24,14 @@ describe("Tests cases for compareTheTriplets", () => {
 
     test("Bob2 earn more points than Anne2", () => {
         expect(compareTheTriplets(anne2, bob2)).toBe("0 3");
+    });
+
+    test("Parameters provided are less than 100", () => {
+        expect(compareTheTriplets(anne3, bob3)).toContain("less than or equal to 100");
+    });
+
+    test("Parameters provided are positive integers", () => {
+        expect(compareTheTriplets(anne4, bob4)).toContain("must be positive");
     });
 
 });
